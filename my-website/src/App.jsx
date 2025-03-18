7c38751db9de29e5af17ba8060692870d0a452a3
may-orca <orca531@icloud.com>
2025-03-18 21:11:59 +0900

    Commit Test

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Git Hub 수정을 해볼거에요.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
